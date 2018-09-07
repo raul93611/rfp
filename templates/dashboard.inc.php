387d9c6f88167ad3fde5491c97600e3a3e999995
raul-trabajo <raul93611@gmail.com>
2018-09-07 15:53:04 -0400

    bug in dashboard fixed

--- templates/dashboard.inc.php
+++ templates/dashboard.inc.php
@@ -1,6 +1,16 @@
 <?php
 if($level == 1){
-  include_once 'templates/admin_dashboard.inc.php';
+  if($current_manager == ''){
+    include_once 'templates/admin_dashboard.inc.php';
+  }else if($current_manager == 'calendar_new_projects'){
+    include_once 'templates/calendar_new_projects.inc.php';
+  }else if($current_manager == 'calendar_projects'){
+    include_once 'templates/calendar_projects.inc.php';
+  }else if($current_manager == 'calendar_my_projects'){
+    include_once 'templates/calendar_my_projects.inc.php';
+  }else if($current_manager == 'calendar_my_tasks'){
+    include_once 'templates/calendar_my_tasks.inc.php';
+  }
 }else if($current_manager == ''){
   include_once 'templates/story_comments.inc.php';
 }else if($current_manager == 'calendar_new_projects'){
